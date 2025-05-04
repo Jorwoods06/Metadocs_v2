6c944d5f0fb080738c50ee05238631c6699c7fc4
Jorge <jorgemulato206@gmail.com>
2025-05-03 21:08:56 -0500

    Sistema de autenticacion hecho

---
+++ app/backend/login/autenticacion.php
@@ -0,0 +1,97 @@
+<?php
+
+
+session_start(); 
+
+require_once '../../helpers/conexion_bd.php';
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST'){
+
+    echo "tu peticion ha sido rechazada";
+
+    exit;
+
+}else{
+
+    if(!isset($_POST['gmail']) && !isset($_POST['contrasena'])){
+       
+        echo "no se puede ejecutar la operación púes faltan datos";
+    
+    }else{
+        
+        $correo_ingresado = strtolower(filter_input(INPUT_POST, 'gmail', FILTER_SANITIZE_EMAIL));
+        $contraseña_ingresado = md5($_POST["contrasena"]);
+
+
+        $sentencia = $conexion_metadocs->prepare("SELECT * FROM `usuarios` WHERE correo=?");
+        $sentencia->bind_param("s", $correo_ingresado);
+        $sentencia->execute();
+        $resultado = $sentencia->get_result();
+
+        $autenticacion = $resultado -> fetch_object();
+
+        if (!$autenticacion) {
+           
+            $_SESSION['denegado'] = 'Acceso denegado';
+           
+            header('Location: ../../../login.php'); 
+           
+            exit();
+        }else{
+            
+            if($contraseña_ingresado != $autenticacion->contraseña) {
+
+                $_SESSION['error'] = 'contraseña incorrecta';
+                
+                header('Location: ../../../login.php');
+                
+                exit();
+
+            } else {
+
+                $rol = $autenticacion->rol;
+                $area = $autenticacion ->id_area;
+                
+                
+                $_SESSION['id_log'] =$id_usuario = $datos->id_usuario;
+                $_SESSION['rol'] =$rol;
+                $_SESSION['area'] =$area;
+            
+            
+
+
+                switch ($rol) {
+                    case 'administrador':
+                        header("Location: ../../vistas/admin/panel_control.php");
+                        break;
+                    case 'visualizador':
+                        header("Location: visualizador.php");
+                        break;
+                    case 'documentador':
+                        header("Location: ../../app/views/documentador.php");
+                        break;
+                    case 'auditor':
+                        header("Location: ../../app/views/auditor.php");
+                        break;
+                    default:
+                        header("Location: index.html");
+                }
+                exit;
+
+            }
+        }
+
+    
+    }
+
+
+
+}
+
+
+
+
+
+
+?>
+

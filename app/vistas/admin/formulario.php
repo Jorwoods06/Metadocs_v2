246da1e292bf509a034a61dfeaec70ff522f6dc2
yellen01 <129990840+yellen01@users.noreply.github.com>
2025-05-16 11:01:28 -0500

    crear_usuario

---
+++ app/vistas/admin/formulario.php
@@ -0,0 +1,10 @@
+<?php include 'panel.html'; ?>
+
+<section id="contenido-principal">
+    <h1>este sera el formulario de creacion del usuario</h1>
+    <form action="crear_usuario"></form>
+</section>
+
+</main>
+</body>
+</html>

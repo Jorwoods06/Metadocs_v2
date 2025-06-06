6e517f6832f0015bc7bac8ab3bbdfb7b63d575ec
santiago llanos cardona <santicardona1424@gmail.com>
2025-06-05 20:38:17 -0500

    interfaz de informacion de usuario

--- app/vistas/admin/informacion_usuario.php
+++ app/vistas/admin/informacion_usuario.php
@@ -1,8 +1,8 @@
 <?php 
 
 require_once '../../helpers/verificacion_roles.php';
-AutorizacionRol('administrador');
 
+AutorizacionRol('administrador');
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -15,11 +15,12 @@ AutorizacionRol('administrador');
     <link rel="stylesheet" href="../../../componentes/css/admin/panel.css">
     <link rel="stylesheet" href="../../../componentes/css/admin/control.css">
     <script src="../../../componentes/js/admin/panel.js"></script>
+    <link rel="stylesheet" href="../../../componentes/css/admin/informacion_usuario.css">
 </head>
 <body>
     <header id="cabezote">
         <i class="bi bi-list" id="menu_opciones"></i>
-        
+
     </header>
 
     <main id="cuerpo">
@@ -35,38 +36,29 @@ AutorizacionRol('administrador');
                     </a>
                 </li>
                 <li class="gestion_usuario">
-                    <a href="#" id="gestion-usuarios">
+                    <a href="#" id="gestion-usuarios" >
                         <i class="bi bi-people"></i>
                         Gestión Usuarios
                     </a>
                     <ul class="sub_menu gestion-submenu" id="sub_menu">
-                        <li>
-                            <a href="../../vistas/admin/creacion_usuario.php">
-                                <i class="bi bi-person-plus"></i>
-                                Crear usuario
-                            </a>
-                        </li>
-                        <li>
-                            <a href="../admin/ver_usuarios.php">
-                                <i class="bi bi-eye"></i>
-                                Ver usuario
-                            </a>
-                        </li>
+                        <li><a href="../../vistas/admin/creacion_usuario.php"><i class="bi bi-person-plus"></i> Crear usuario</a></li>
+                        <li><a href="../admin/ver_usuarios.php"><i class="bi bi-eye"></i> Ver usuario</a></li>
                     </ul>
                 </li>
                 <li>
                     <a href="../admin/admin_reporte.php">
                         <i class="bi bi-file-earmark-text"></i>
-                        Reportes
+                        Reportes 
                     </a>
                 </li>
-
+                
+                    <!-- cerrado sesion -->  
                 <li class="gestion-usuarios">
                     <a href="#" id="cerrado-usuarios">
                         <i class="bi bi-person"></i>
                         Usuario
                     </a>
-                       <ul class="sub_menu usuario-submenu" id="sub_menu">
+                    <ul class="sub_menu usuario-submenu" id="sub_menu">
                         <li><form action="../../backend/login/cerrar_sesion.php" method="post"><button type="submit"><i class="bi bi-box-arrow-left"></i>Cerrar sesion</button></form></li>
                         <li><a href="../../vistas/admin/informacion_usuario.php"><i class="bi bi-info-circle"></i> Info usuario</a></li>
                         <li><a href=""><i class="bi bi-key-fill"></i> Cambiar contraseña</a></li>
@@ -77,18 +69,57 @@ AutorizacionRol('administrador');
 
                 <li class="solo_mobil">
                     <a href="#" id="solo_mobil">
-                        <i class="bi bi-arrow-left"></i>
+                        <i class="bi bi-arrow-left-circle"></i>
                         Volver
                     </a>
                 </li>
             </ul>
         </nav>
        
-        <section id="admin-contenido" class="admin">
-            <h1>Panel control</h1>
+        <section class="contenido-usuario">
+            <h1 class="titulo-usaurio">Informacion del Usuario</h1>
+            <div class="info-usuario">
+                <div class="info-usuarios">
+                    <img src="../../../componentes/img/usuario.png" alt="logo de usuario" class="avatar-usuario">
+                    <div class="nombre-usuario">Jorge Admin</div>
+                </div>
+            
+                
+                
+
+                <div class="contenedor-datos">
+                    <div class="datos">
+                        <label>Nombre</label>
+                        <div class="valor">jorge</div>  
+                    </div>
+                    <div class="datos">
+                        <label>area</label>
+                        <div class="valor">Administración</div>
+                    </div>
+                    <div class="datos">
+                        <label>Apellido</label>
+                        <div class="valor">admin</div>
+                    </div>
+                    <div class="datos">
+                        <label>Rol</label>
+                        <div class="valor">Administrador</div>
+                    </div>
+                    <div class="datos">
+                        <label>Correo Elctronico</label>
+                        <div class="valor">dg24004@gmail.com</div>
+                    </div>
+                    <div class="datos">
+                        <label>Descripcion laboral</label>
+                        <div class="valor">Jorge se desempeña como administrador en gestión documental, encargado de organizar, clasificar y custodiar los documentos tanto físicos como digitales de la empresa. </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
-      
+
+
+
+
     </main>
 </body>
 </html>

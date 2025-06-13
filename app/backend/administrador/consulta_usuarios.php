965080707616772081a6e65d83bce972edc45ba1
Jorge <jorgemulato206@gmail.com>
2025-06-13 14:52:22 -0500

    error

--- app/backend/administrador/consulta_usuarios.php
+++ app/backend/administrador/consulta_usuarios.php
@@ -2,8 +2,8 @@
 
 require_once '../../helpers/conexion_bd.php';
 
-$sentencia = "SELECT usuarios.nombres, usuarios.correo, usuarios.rol, area_acceso.nombre AS area FROM usuarios JOIN area_acceso ON usuarios.id_area = area_acceso.id_area WHERE correo = 'activo';";
-$res_correo = ["correo"]
+$sentencia = "SELECT usuarios.nombres, usuarios.correo, usuarios.rol, area_acceso.nombre AS area FROM usuarios JOIN area_acceso ON usuarios.id_area = area_acceso.id_area WHERE estado = 'activo';";
+
 $resultado = $conexion_metadocs->query($sentencia);
 
 
